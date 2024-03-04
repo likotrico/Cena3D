99e057592a092d569329a2db92132395b5375c26
slottwo <41028091+slottwo@users.noreply.github.com>
2024-03-04 19:38:55 -0300

    Basic formatting

--- input.c
+++ input.c
@@ -1,49 +1,48 @@
-#include  <stdio.h>
 #include <windows.h>
 #include <GL/freeglut.h>
-
+#include <stdio.h>
 
 void keyPressed(unsigned char key, int x, int y)
 {
-    //SELECIONA BULE
-    if(key == '1')
+    // SELECIONA BULE
+    if (key == '1')
     {
         printf("Bule selecinado!\n");
     }
 
-    //SELECIONA BOLA
-    if(key == '2')
+    // SELECIONA BOLA
+    if (key == '2')
     {
         printf("Bola selecinada!\n");
     }
 
-    //SELECIONA TORO
-    if(key == '3')
+    // SELECIONA TORO
+    if (key == '3')
     {
         printf("Toro selecinado!\n");
     }
 }
 
-void keyPressed_special(int key, int x, int y){
+void keyPressed_special(int key, int x, int y)
+{
 
-    if(key == GLUT_KEY_LEFT)
+    if (key == GLUT_KEY_LEFT)
     {
         printf("Movendo esquerda!\n");
     }
 
-    if(key == GLUT_KEY_RIGHT)
+    if (key == GLUT_KEY_RIGHT)
     {
         printf("Movendo direita!\n");
     }
 
-    if(key == GLUT_KEY_UP)
+    if (key == GLUT_KEY_UP)
     {
         printf("Movendo cima!\n");
     }
 
-    if(key == GLUT_KEY_DOWN)
+    if (key == GLUT_KEY_DOWN)
     {
         printf("Movendo baixo!\n");
     }
-
 }
